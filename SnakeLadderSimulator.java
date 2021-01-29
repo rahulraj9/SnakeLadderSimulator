b4c3d3dc1eeeabc38ef3305c1372557443c3abb3
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-30 00:51:19 +0530

    [Rahul]StartGame GameStartingAtPos0

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,8 +1,9 @@
 public class SnakeLadderSimulator {
-
+	private static final int PLAYER = 1;
 	public static void main(String[] args) {
+		int position = 1;
 		System.out.println(":::::::::Welcome To Snake Ladder Simulator:::::::::");
-
+		System.out.println("Current Player: "+PLAYER +" Position is "+position);
 	}
 
 }

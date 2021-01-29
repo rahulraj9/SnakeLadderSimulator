623899b347bea57cb98a326a34bd2696b87bcaf9
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-30 01:00:04 +0530

    [Rahul]GamePlayOption CheckingForGamePlayOption

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,8 +1,28 @@
 public class SnakeLadderSimulator {
+	private static final int LADDER = 1;
+	private static final int SNAKE = 2;
+	
 	public static void main(String[] args){
 		Random random = new Random();
-		int dice = random.nextInt(6) + 1;
-		System.out.println("Dice Face: "+dice);
-	}
-}
+		
+		int diceRolling = random.nextInt(6) + 1;		
+		int optionForPlaying = random.nextInt(3);
+		int position = 0;
+		
+		System.out.println("Previous Position: "+position);
+		
+		switch(optionForPlaying){
+		case LADDER :
+			position += diceRolling;
+			break;
+		case SNAKE :
+			position -= diceRolling;
+			break;
+		default :
+			System.out.println("You Are In NOPLAY");
+		}
+		
+		System.out.println("Dice Face: "+diceRolling);
+		System.out.println("New Position: " +position);
+	}}
 

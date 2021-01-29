8cd60fdcd0e8eba9dc2e6b23d4be499c6e3f300b
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-30 00:55:57 +0530

    [Rahul]Dice RollingDice

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,10 +1,8 @@
 public class SnakeLadderSimulator {
-	private static final int PLAYER = 1;
-	public static void main(String[] args) {
-		int position = 1;
-		System.out.println(":::::::::Welcome To Snake Ladder Simulator:::::::::");
-		System.out.println("Current Player: "+PLAYER +" Position is "+position);
+	public static void main(String[] args){
+		Random random = new Random();
+		int dice = random.nextInt(6) + 1;
+		System.out.println("Dice Face: "+dice);
 	}
-
 }
 

dc2a2de14a7e544dc1731e27fb6defed09220ac4
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-30 01:04:50 +0530

    [Rahul]Position RepeateUntilReachThePosition

--- SnakeLadderSimulator.java
+++ SnakeLadderSimulator.java
@@ -1,28 +1,43 @@
 public class SnakeLadderSimulator {
 	private static final int LADDER = 1;
 	private static final int SNAKE = 2;
-	
-	public static void main(String[] args){
-		Random random = new Random();
-		
-		int diceRolling = random.nextInt(6) + 1;		
-		int optionForPlaying = random.nextInt(3);
-		int position = 0;
-		
-		System.out.println("Previous Position: "+position);
-		
-		switch(optionForPlaying){
-		case LADDER :
-			position += diceRolling;
-			break;
-		case SNAKE :
-			position -= diceRolling;
-			break;
-		default :
-			System.out.println("You Are In NOPLAY");
-		}
-		
-		System.out.println("Dice Face: "+diceRolling);
-		System.out.println("New Position: " +position);
-	}}
+	private static final int NO_PLAY = 0;
 
+	public static void main(String[] args) {
+
+		int position = 1;
+		System.out.println("PREVIOUSPOSITION\tDICEROLLING\tPLAYINGOPTION\tNEWPOSITION\n");
+
+		while(position < 100)
+		{
+			Random random = new Random();
+
+			int diceRolling = random.nextInt(6) + 1;
+			int optionForPlaying = random.nextInt(3);
+
+			System.out.print(position+" \t\t\t");
+			System.out.print(diceRolling+"\t\t");
+
+			switch(optionForPlaying){
+			case LADDER :
+				position += diceRolling;
+				System.out.print("Ladder\t\t"+position+"\n");
+				break;
+
+			case SNAKE :
+				position -= diceRolling;
+				if(position < 0)
+					position = 0;
+				System.out.print("Snake\t\t"+position+"\n");
+				break;
+
+			case NO_PLAY:
+				System.out.print("No-play\t\t"+position+"\n");
+
+			default :
+
+			}
+		}
+		System.out.println("player reaches position:-" +position);
+	}
+}
